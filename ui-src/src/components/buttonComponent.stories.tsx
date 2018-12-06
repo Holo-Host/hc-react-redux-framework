ff75c521be3d782f21c60715433881313ba1c221
zo-el <joelulahanna@gmail.com>
2018-12-06 13:04:11 -0600

    small bug fixes

--- ui-src/src/components/buttonComponent.stories.tsx
+++ ui-src/src/components/buttonComponent.stories.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react'
 import { storiesOf } from '@storybook/react'
 // import { action } from '@storybook/addon-actions'
-import { specs } from 'storybook-addon-specifications'
+// import { specs } from 'storybook-addon-specifications'
 // import { withNotes } from '@storybook/addon-notes'
 import ButtonComponent from './exampleComponent'
 // import { autoCompleteProfileFieldTests } from './exampleComponent.test'
